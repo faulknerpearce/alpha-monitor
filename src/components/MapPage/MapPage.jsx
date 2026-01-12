41202cbee3cdf5765f91f916877dc323d355ef55
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2026-01-11 17:09:46 -0700

    Add MapPage component for dedicated map view
    - Add MapPage.jsx and MapPage.css
    - Create dedicated page for GlobalMap component
    - Separate map functionality from dashboard layout

---
+++ src/components/MapPage/MapPage.jsx
@@ -0,0 +1,12 @@
+import GlobalMap from '@components/panels/GlobalMap/GlobalMap'
+import './MapPage.css'
+
+const MapPage = () => {
+    return (
+        <div className="map-page">
+            <GlobalMap />
+        </div>
+    )
+}
+
+export default MapPage
